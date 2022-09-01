c09115a4028fa20c5c5fe001deb0c20e94a45155
Vitória Lopes <vitoria.lopes@55pbx.com.br>
2022-09-01 09:28:49 -0300

    new: request github oauth

--- src/src/pages/login/index.tsx
+++ src/src/pages/login/index.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './styles.css';
+
+const GITHUB_URL = 'https://github.com';
+const OAUTH_CLIENT = 'fea016b8dcb02c029817'; // identificador da aplicação
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function login(e: FormEvent) {
+    e.preventDefault();
+
+    if (username === 'heyitoria.lopes@gmail.com' && password === '1234') {
+      return navigate('/main');
+    }
+
+    alert('Credenciais incorretas. Tente novamente. ');
+  }
+
   return (
-    <h1>Login</h1>
+    <form onSubmit={login}>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder='Login'
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='Password'
+      />
+      <button type='submit'>Login</button>
+      <a href={`${GITHUB_URL}/login/oauth/authorize?client_id=${OAUTH_CLIENT}`}>
+        Login com GitHub
+      </a>
+    </form>
   );
 }
 
